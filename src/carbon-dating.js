fb646bda08f279670248fb06b167f0dac7c244fe
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-04-18 00:15:43 +0300

    feat: complite carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || sampleActivity <= 0 || isNaN(sampleActivity) || sampleActivity > MODERN_ACTIVITY) {
+    return false;
+  }
+  const carbon = 0.693 / HALF_LIFE_PERIOD;
+  const time = Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / carbon);
+  return time;
 }
 
 module.exports = {
