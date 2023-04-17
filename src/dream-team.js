8d8a59ef854009d34fcb2fd7e7b6a159d62d2b9e
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-04-18 00:25:23 +0300

    feat: complite dreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let teamName = '';
+
+  members.forEach(unit => {
+    if (typeof unit === 'string') {
+      let firstLetter = unit.trim()[0].toUpperCase();
+      teamName += firstLetter;
+    }
+  });
+
+  return teamName.split('').sort().join('');
 }
 
 module.exports = {
